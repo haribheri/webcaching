0c1c0cf3b25f1a5932499cf7ede31cd0e7b6683d
srihari <srihari@bheri>
2016-03-05 12:29:05 +0530

    first commit

--- src/webcaching/Webcache.java
+++ src/webcaching/Webcache.java
@@ -1,24 +1,13 @@
-
 package webcaching;
+
 import java.util.*;
 import java.io.*;
 
-public class Webcache {
-    
+public class Webcache 
+{   
+    LinkedList<CacheEntry> list=new LinkedList<CacheEntry>();
     private static Webcache instance=null;
     private Webcache()
-    {
-        
-    }
-    public static Webcache getInstance()
-    {
-        if(instance==null)
-        {
-            instance = new Webcache(); 
-        }
-    }
-    
-    public static void main(String arr[] )
     {
         CacheEntry c1=new CacheEntry(1,true);
         CacheEntry c2=new CacheEntry(2,true);
@@ -26,13 +15,18 @@ public class Webcache {
         CacheEntry c4=new CacheEntry(4,false);
         CacheEntry c5=new CacheEntry(5,true);
         
-        LinkedList<CacheEntry> list=new LinkedList<CacheEntry>();
         list.add(c1);
         list.add(c2);
         list.add(c3);
         list.add(c4);
         list.add(c5);
-       
     }
-    
+    public static Webcache getInstance()
+    {
+        if(instance==null)
+        {
+            instance = new Webcache(); 
+        }
+      return instance;
+            }          
 }
