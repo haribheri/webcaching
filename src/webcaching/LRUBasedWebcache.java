5b77e996396063aedf60a4ee0ba4ca3ca586581e
haribheri <bheri.bheri@gmail.com>
2016-04-14 16:35:03 +0530

    recent modification

--- src/webcaching/LRUBasedWebcache.java
+++ src/webcaching/LRUBasedWebcache.java
@@ -11,9 +11,50 @@ public class LRUBasedWebcache {
         this.list=new LinkedList<LRUObject>();
         map=new HashMap<Integer,LRUObject>();
     }
-    public void put()
+    public boolean checkPage(LRUObject o)
+    {
+        if(map.containsKey(o))
+        {
+            updateCache();
+            return true;
+        }
+        else
+        {
+            put(o);
+            return false;
+        }
+    }
+            
+    private void put(LRUObject o)
+    {
+        if(isCacheAvilable())
+        {
+            list.add(o);
+            map.put(Integer.SIZE, o);
+            updateCache();
+        }
+        else
+        {
+            deleteCacheEntry();
+            list.add(o);
+            map.put(Integer.SIZE, o);
+            
+        }
+        
+    }
+    private boolean isCacheAvilable()
+    {
+        if(list.size()==100)
+            return false;
+        else
+            return true;
+    }
+    private void updateCache()
     {
         
     }
-    
+    private void deleteCacheEntry()
+    {
+        
+    }
 }
