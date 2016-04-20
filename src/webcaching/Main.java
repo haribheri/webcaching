fc8551aa8e06d893e0eba4a711e491344bfce9ce
haribheri <bheri.bheri@gmail.com>
2016-04-20 12:19:58 +0530

    recent modification

--- src/webcaching/Main.java
+++ src/webcaching/Main.java
@@ -1,6 +1,8 @@
 package webcaching;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main extends Thread
@@ -9,11 +11,23 @@ public class Main extends Thread
     Webcaching webcaching;
     PageRequestEvent pageRequestEvent;
     Client client;
+    int numberOfClients;
    
     public Main()
     {
-        pageRequestEventsQueue = new LinkedList<PageRequestEvent>();
-        client = new Client(pageRequestEventsQueue);
+       Scanner file=null;
+       try
+       {
+           file=new Scanner(new FileInputStream("G:\\Java\\Webcaching\\src\\input.txt"));           
+       }
+       catch(FileNotFoundException e)
+       {
+           System.out.println("unable to locate file");
+           System.exit(0);      
+       }
+       this.numberOfClients=file.nextInt();
+       pageRequestEventsQueue = new LinkedList<PageRequestEvent>();
+       client = new Client(pageRequestEventsQueue);
         
         for(int i=0;i<client.numberOfPages;i++)
         {
